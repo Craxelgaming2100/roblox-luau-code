669199939a75bd72d26a608639ada39ba233e4ec
Craxelgaming2100 <137681003+Craxelgaming2100@users.noreply.github.com>
2024-01-13 22:48:06 +0700

    Create client.lua

---
+++ server-client/join-msg/client.lua
@@ -0,0 +1,4 @@
+game.ReplicatedStorage.DisplaySysMsg.OnClientEvent:Connect(function(message: string)
+	game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage("<font color='#32a852'>[System]</font> " .. message)
+	print("new system message:", message)
+end)
