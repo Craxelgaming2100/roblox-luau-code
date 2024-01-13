d921b079a13cbf2a60f493be63450260c8aba4c3
Craxelgaming2100 <137681003+Craxelgaming2100@users.noreply.github.com>
2024-01-13 22:46:01 +0700

    Update server.lua

--- server-client/join-msg/server.lua
+++ server-client/join-msg/server.lua
@@ -1,1 +1,5 @@
+local event = game.ReplicatedStorage.SendMessage -- Replace with your actual event
 
+game.Players.PlayerAdded:Connect(function(plr)
+  event:FireAllClients(plr.Name.." has joined the game!")
+end)
