cb9a8844fe606f78e2770f537d66c7cfa10078d3
NotCraxel <137681003+Craxelgaming2100@users.noreply.github.com>
2025-05-13 09:30:11 +0700

    Update client.lua

--- server-client/join-msg/client.lua
+++ server-client/join-msg/client.lua
@@ -1,4 +1,4 @@
 game.ReplicatedStorage.SendMessage.OnClientEvent:Connect(function(message: string)
-	game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage("<font color='#32a852'>[System]</font> " .. message)
+	game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage("<font color='#32a852'>[System]</font> " .. message) -- you can change the font color :P
 	print("new system message:", message)
 end)
