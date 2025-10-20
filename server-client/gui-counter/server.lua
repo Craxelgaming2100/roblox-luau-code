bc32f461dd23f150e8c7ed658f7d544d4755dcfd
NotCraxel <137681003+Craxelgaming2100@users.noreply.github.com>
2025-10-20 18:36:06 +0700

    Update server.lua

--- server-client/gui-counter/server.lua
+++ server-client/gui-counter/server.lua
@@ -1,6 +1,6 @@
 local event = game.ReplicatedStorage.IncreaseCounter -- replace with your event
 
-while wait(5) do
+while wait(5) do -- every 5 seconds, there is a 1 in 6 chance to count the counter up.
   if math.random(1, 6) == 6 then
       event:FireAllClients()
   end
