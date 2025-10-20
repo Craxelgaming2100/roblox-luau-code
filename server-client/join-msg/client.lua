faee988859a9113e6dc4093eb708ae8203ae237f
NotCraxel <137681003+Craxelgaming2100@users.noreply.github.com>
2025-10-20 18:45:31 +0700

    Update client.lua

--- server-client/join-msg/client.lua
+++ server-client/join-msg/client.lua
@@ -1,4 +1,7 @@
 game.ReplicatedStorage.SendMessage.OnClientEvent:Connect(function(message: string)
-	game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage("<font color='#32a852'>[System]</font> " .. message) -- you can change the font color :P
-	print("new system message:", message)
+	local nameColor = Color3.fromRGB(50,168,82) --#32a852
+	local messageColor = Color3.new(1,1,1) --#ffffff
+	local includeColon = false       --RBXGeneral is the default channel.      -- ToHex() converts a Color3 to hex.            -- includeColon check
+	game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage("<font color='"..nameColor:ToHex().."'>[System]"..(if includeColon then ":" else "").."</font> <font color='"..messageColor:ToHex().."'>"..message.."</font>") -- you can change the font color :P
+	--print("new system message:", message)
 end)
