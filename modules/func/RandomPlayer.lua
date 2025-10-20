ab76f5e9ac6dbb17cd0298630fc19e0ba2ef4b73
NotCraxel <137681003+Craxelgaming2100@users.noreply.github.com>
2025-10-20 18:50:00 +0700

    Update RandomPlayer.lua; Add function description

--- modules/func/RandomPlayer.lua
+++ modules/func/RandomPlayer.lua
@@ -1,3 +1,4 @@
-return function()
+--- Returns a random Player that is currently in the game.
+return function(): Player
   return game.Players:GetPlayers()[math.random(1,#game.Players:GetPlayers())]
 end
